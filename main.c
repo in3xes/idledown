944e8aea8bae13b9817a42e9c781f87234a97012
Pradeepkumar Gayam <deepu.aprjc@gmail.com>
2010-02-24 17:58:49 +0530

    No message

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
-/*(C) 2010 Gayam Pradeepkumar <deepu.aprjc@gmail.com>		*/
+/*A daemon to shutdown idle computer				*\
+*								*
+\*(C) 2010 Pradeepkumar Gayam<deepu.aprjc@gmail.com>		*/
+
+/*
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License with
+  the Debian GNU/Linux distribution in file /usr/doc/copyright/GPL;
+  if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+  Suite 330, Boston, MA  02111-1307  USA
+*/
 
 #include "id.h"
 
@@ -6,9 +25,14 @@ int main() {
 	int time1 = 0;
 	while(1) {
 		time1 = idletime();
-		printf("Idle time is :%i\n", time1);
-		sleep(1);
-	}
+		if(time1 >= 19999) {
+			printf("Shutting down\n");
+			system("/sbin/init 0");
+		}
+		else {
+			continue;
+		}
 
 	return 1;
+	}
 }
